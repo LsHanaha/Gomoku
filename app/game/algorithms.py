dce6fb27928b9265fc06b88a7c9e97f685a4289c
LsHanaha <kirik193@yandex.ru>
2022-03-26 17:59:29 +0300

    add algo

--- app/game/algorithms.py
+++ app/game/algorithms.py
@@ -1,18 +1,21 @@
 import random
 from app.game.game_interfaces import RobotGame
+import algo_module
 
 from app.schemas import game_schemas
 
 
-async def _min_max(game: RobotGame, depth=None) -> game_schemas.Point:
-    col = random.randint(0, 18)
-    row = random.randint(0, 18)
+async def _min_max(game: RobotGame) -> game_schemas.Point:
+    # col = random.randint(0, 18)
+    # row = random.randint(0, 18)
+    # TODO place where I added your script
+    move = algo_module.get_moves(game.field, game.curr_player, game.algorithm_depth)
     game.last_robot_time = random.randint(100, 1000)
     game.debug_data = None
     # game.debug_data = [[0, 0],[1, 1],[2, 2],[3, 3],[4, 4]]
     return game_schemas.Point(
-        row=row,
-        col=col,
+        row=move[0],
+        col=move[1],
         uuid=game.uuid
     )
 
